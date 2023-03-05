04676f38bd79f10829f63d20eccfab9eb2891b6e
newbz521 <lawrence.yee1991@gmail.com>
2023-03-05 18:39:34 -0500

    image maintenance

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import FittingRoom from './components/FittingRoom'
-import Navbar from './components/Navbar'
-import './index.css'
+import FittingRoom from "./components/FittingRoom";
+import Navbar from "./components/Navbar";
+import "./index.css";
 
 function App() {
   return (
-    <div>
+    <div className="main-container">
+      {/* <div className="mainImage"></div> */}
       <Navbar />
       <FittingRoom />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
